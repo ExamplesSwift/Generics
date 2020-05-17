7bca4bb61ab6783f721a20cb5b862598540ffda9
Henry AT <yiperu@gmail.com>
2020-05-17 18:18:45 -0400

    create a simple stack of String type

--- GenericsPlayground.playground/Contents.swift
+++ GenericsPlayground.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var str = "Hello, playground"
+// this a example of simple Stack
+struct Stack: CustomStringConvertible {
+  
+  private var items: [String]
+  
+  var description: String {
+    return "\(items)"
+  }
+  
+  var isEmpty : Bool {
+    return items.isEmpty
+  }
+  
+  init() {
+    items = []
+  }
+  
+  mutating func pop() -> String? {
+    return !isEmpty ? items.removeLast() : nil
+  }
+  
+  mutating func push(item: String) {
+    items.append(item)
+  }
+}
+
+var stackOfName = Stack()
+stackOfName.push(item: "Mary")
+stackOfName.push(item: "Susan")
+stackOfName.push(item: "George")
+stackOfName.push(item: "Jessica")
+
+while let name = stackOfName.pop() {
+  print("popping name: \(name)")
+}
